45f8ae449d1dae2e5941801747bbc396789d8ba4
thiagomendesdeveloper <thiagomendessilva77@gmail.com>
2021-12-24 01:29:42 -0200

    api

--- src/index.tsx
+++ src/index.tsx
@@ -8,41 +8,44 @@ type filmes = {
     id: number
 }
 
-const data = [
-    {   
-        id: 1,
-        title: 'Avengers',
-        ano: '1997'
-    },
-    {
-        id: 2,
-        title: 'Home Aranha',
-        ano: '2002'
-    },
-    {
-        id: 3,
-        title: 'Homem de Ferro',
-        ano: '2005'
-    }
-]
+interface Movies {
+    id: number,
+    title: string,
+    backdrop_path: string
+}
+
+const api_key = "e61c352783109ca0da4d5730b18b36a5"
+const api_base = "https://api.themoviedb.org/3"
+const base_img = "https://www.themoviedb.org/t/p/w220_and_h330_face"
 
 export default function Home(){
 
-    useEffect( ()=> {
-        
-    })
+    const [ Movies , setMovies] = useState<Movies>()
+    
+    useEffect (() => {
+        const fetchbase = async() => {
+            const req = await fetch(`${api_base}/discover/movie?api_key=${api_key}&language=pt-br&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`)
+            const json = await req.json()
+            setMovies( json )
+        }
+        fetchbase()
+    },[])
+
+    // console.log(Movies)
     
     return(
         <View style={styles.container}>
             <Text style={styles.title}>Filmes</Text>
-            <View style={styles.content}>
-                {data.map(function(e){
+             <View style={styles.content}>
+                {console.log(Movies?.title)}
+                 {/* {Movies?.map( (e) => {
                     <View key={e.id} style={styles.card}>
                         <Image style={styles.thumb} source={require('../assets/avengers.jpg')} />
                         <Text style={styles.name}>{e.title}</Text>
-                        <Text style={styles.ano}>{e.ano}</Text>
-                    </View>
-                })}
+                        <Text style={styles.ano}>{e.backdrop_path}</Text>
+                    </View> */}
+                {/* })}   */}
+                <Text style={styles.name}>{Movies?.title}</Text>
             </View>
         </View>
     )
